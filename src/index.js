fb5cb698af5dd7f8c9a843ee84963c5c9d4fd1f3
EvgChe <chetkova@list.ru>
2018-03-07 12:40:14 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,80 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+        this.eq_array = [];
+        this.eq_array.push(initialValue);
+        this.result = 0;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    add(number) {
+        //this.value += number;
+        this.eq_array.push('+');
+        this.eq_array.push(String(number));
+        this.result = this.solve();
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    subtract(number) {
+        //this.value -= number;
+        this.eq_array.push('-');
+        this.eq_array.push(String(number));
+        this.result = this.solve();
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    multiply(number) {
+        //this.value *= number;
+        this.eq_array.push('*');
+        this.eq_array.push(String(number));
+        this.result = this.solve();
+        return this;
+    }
+
+    devide(number) {
+        //this.value /= number;
+        this.eq_array.push('/');
+        this.eq_array.push(String(number));
+        this.result = this.solve();
+        return this;
+    }
+
+    pow(number) {
+        var arg;
+        arg = this.eq_array.pop();
+        var count_brackets = 1;
+        if(arg === ')'){
+            while(arg == ')') {
+                count_brackets++;
+                arg = this.eq_array.pop();
+            }
+            this.eq_array.push('Math.pow(');
+            this.eq_array.push(arg);
+            this.eq_array.push(',');
+            this.eq_array.push(String(number));
+            while(count_brackets>0){
+                this.eq_array.push(')');
+                count_brackets--;
+            }
+        }
+        else {
+            this.eq_array.push('Math.pow(');
+            this.eq_array.push(arg);
+            this.eq_array.push(',');
+            this.eq_array.push(String(number));
+            this.eq_array.push(')');
+            this.result = this.solve();
+        }
+        return this;
+    }
+
+    solve(){
+        return eval(this.eq_array.join(''));
+    }
+
+    valueOf() {
+        return this.result;
+    }
+    
 }
 
 module.exports = SmartCalculator;
